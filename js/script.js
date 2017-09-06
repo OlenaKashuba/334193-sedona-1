93e41206132e37d83bd6df05d1b4a135702664b7
OlenaKashuba <amely.jazz@gmail.com>
2017-09-06 15:56:39 +0100

    JS, interactive map

---
+++ js/script.js
@@ -0,0 +1,5 @@
+var searchButton = document.querySelector(".btn-hotel-search");
+var form = document.querySelector(".hotel-search-form");
+searchButton.addEventListener("click", function(){
+	form.classList.toggle("hotel-form-hide");
+	});
